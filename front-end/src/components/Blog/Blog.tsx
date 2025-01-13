0d0e245f6196188d4f1edf762f159750896a33fb
phadhiil001 <phadhiil001@gmail.com>
2025-01-12 22:59:46 -0600

    Completed Blog component

--- front-end/src/components/Blog/Blog.tsx
+++ front-end/src/components/Blog/Blog.tsx
@@ -1,10 +1,73 @@
-import React from 'react'
-import './Blog.css'
+import { blog1, blog2, blog3 } from "../../assets";
+import "./Blog.css";
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
-}
+	return (
+		<section
+			id='blog'
+			className='pink'>
+			<div className='wrapper'>
+				<h2 className='light'>Latest Posts</h2>
+				<div className='content-container'>
+					<div className='post'>
+						<div className='tag'>DAW</div>
+						<a href='#'>
+							<img
+								src={blog1}
+								alt=''
+							/>
+						</a>
 
-export default Blog
+						<a href='#'>
+							<h3 className='post-title'>
+								How to use Drum machine in Logic Pro X
+							</h3>
+						</a>
+					</div>
+
+					<div className='post'>
+						<div className='tag'>DAW</div>
+						<a href='#'>
+							<img
+								src={blog2}
+								alt=''
+							/>
+						</a>
+
+						<a href='#'>
+							<h3 className='post-title'>
+								How to use Drum machine in Logic Pro X
+							</h3>
+						</a>
+					</div>
+
+					<div className='post'>
+						<div className='tag'>DAW</div>
+						<a href='#'>
+							<img
+								src={blog3}
+								alt=''
+							/>
+						</a>
+
+						<a href='#'>
+							<h3 className='post-title'>
+								How to use Drum machine in Logic Pro X
+							</h3>
+						</a>
+					</div>
+				</div>
+
+				<div className='btn-container'>
+					<a
+						href='#'
+						className='all-posts-btn'>
+						All Posts
+					</a>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Blog;
