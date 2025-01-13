fcdc74c401bb802ada19d087c272df12d033c18a
phadhiil001 <phadhiil001@gmail.com>
2025-01-12 22:22:32 -0600

    Styled topics component

--- front-end/src/components/Topics/Topics.tsx
+++ front-end/src/components/Topics/Topics.tsx
@@ -19,7 +19,7 @@ const Topics = () => {
 			<div className='wrapper'>
 				<h2>What will you learn</h2>
 				<div className='content-container'>
-					<ul>
+					<ul className='topics-list'>
 						<li onMouseEnter={() => setCurrentImage(frequencies)}>
 							What are frequencies?
 						</li>
